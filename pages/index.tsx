3b1e4096f6faeb74712b7eb7544781725372851d
Fery Ardian Syah <feryardsh123@gmail.com>
2023-03-21 16:13:36 +0700

    Fix: Link to login version 1.0.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import { taskType } from '@/utils/typeTask';
 import { HiPlus } from 'react-icons/hi';
 import { useQuery } from 'react-query';
 import { BeatLoader } from 'react-spinners';
+import Link from 'next/link';
 
 const Home = () => {
   const { isLoading, data } = useQuery('todo', FETCH_SERVICE.getTodo);
@@ -44,33 +45,41 @@ const Home = () => {
         notyet={getIsDone?.length || 0}
       />
       <div className="mt-52 flex items-center flex-col gap-7">
-        <div className="flex justify-between items-end w-full">
-          <span className="ml-10">{getTask?.length} Hail</span>
-          <button
-            className="bg-primary rounded-l-full shadow-[0_4px_10px_rgb(0,0,0,0.25)] flex items-center px-3 py-2 text-xs hover:bg-primary/75"
-            onClick={handleClickModal}
-          >
-            <HiPlus /> Tambah Kegiatan
-          </button>
-        </div>
-        {isLoading && (
-          <div className="mx-auto">
-            <BeatLoader color="#C86D00" />
-          </div>
+        {!localUser.username ? (
+          <Link className="underline text-blue-800" href={'/login'}>
+            Yuk Login Terlebih Dahulu
+          </Link>
+        ) : (
+          <>
+            <div className="flex justify-between items-end w-full">
+              <span className="ml-10">{getTask?.length} Hail</span>
+              <button
+                className="bg-primary rounded-l-full shadow-[0_4px_10px_rgb(0,0,0,0.25)] flex items-center px-3 py-2 text-xs hover:bg-primary/75"
+                onClick={handleClickModal}
+              >
+                <HiPlus /> Tambah Kegiatan
+              </button>
+            </div>
+            {isLoading && (
+              <div className="mx-auto">
+                <BeatLoader color="#C86D00" />
+              </div>
+            )}
+            {getTask?.map((item: taskType) => (
+              <Task
+                key={item._id}
+                _id={item._id}
+                value={item.value}
+                done={item.done}
+                username={item.username}
+                created={item.created}
+                updated={item.updated}
+                setGetId={setGetId}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+          </>
         )}
-        {getTask?.map((item: taskType) => (
-          <Task
-            key={item._id}
-            _id={item._id}
-            value={item.value}
-            done={item.done}
-            username={item.username}
-            created={item.created}
-            updated={item.updated}
-            setGetId={setGetId}
-            setOpenModal={setOpenModal}
-          />
-        ))}
       </div>
     </main>
   );
